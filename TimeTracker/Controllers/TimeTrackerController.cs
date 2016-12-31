bf8c8657090420e73896f5bb3d7858a83dd8e865
prabhata kumar maharana <maharanakumar2@outlook.com>
2016-12-31 23:03:12 +0530

    Add TimeTracker Controller

---
+++ TimeTracker/Controllers/TimeTrackerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeTracker.Controllers
+{
+    public class TimeTrackerController : Controller
+    {
+        // GET: TimeTracker
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
